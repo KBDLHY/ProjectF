1165ff260e8bff8d254aa1be7973c345905443f6
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2025-06-23 17:07:15 +0900

    프로젝트의 모든 Gameplay Ability가 상속받아야 하는 최상위 추상 클래스 생성 - 어빌리티 활성화 방법을 정의하는 enum class 구현 및 변수 선언

---
+++ Source/ProjectF/AbilitySystem/Abilities/BaseGameplayAbility.cpp
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseGameplayAbility.h"
+
+UBaseGameplayAbility::UBaseGameplayAbility()
+{
+	ReplicationPolicy = EGameplayAbilityReplicationPolicy::ReplicateNo;
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+	NetExecutionPolicy = EGameplayAbilityNetExecutionPolicy::LocalPredicted;
+	NetSecurityPolicy = EGameplayAbilityNetSecurityPolicy::ClientOrServer;
+	
+	ActivationPolicy = EAbilityActivationPolicy::Manual;
+}
---
+++ Source/ProjectF/AbilitySystem/Abilities/BaseGameplayAbility.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "BaseGameplayAbility.generated.h"
+
+/**
+ * 어빌리티를 활성화하는 방법을 정의하는 enum class
+ */
+UENUM(BlueprintType)
+enum class EAbilityActivationPolicy : uint8
+{
+	// 코드를 통해 수동으로 어빌리티 활성화
+	Manual,
+
+	// 입력이 들어오면 어빌리티 활성화 시도
+	OnInputTriggered,
+
+	// 입력이 켜진 동안 계속해서 어빌리티 활성화 시도
+	WhileInputActive,
+
+	// Avatar Actor가 ASC에 등록될 때 어빌리티 활성화 시도
+	OnGiveOrSpawn
+};
+
+/**
+ * 프로젝트의 모든 Gameplay Ability가 상속받아야 하는 최상위 추상 클래스
+ */
+UCLASS(Abstract)
+class PROJECTF_API UBaseGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UBaseGameplayAbility();
+
+	EAbilityActivationPolicy GetActivationPolicy() const { return ActivationPolicy; }
+
+protected:
+	UPROPERTY(EditDefaultsOnly)
+	EAbilityActivationPolicy ActivationPolicy;
+};
