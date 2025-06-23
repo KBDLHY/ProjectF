9784821f2e4a2e5f5186bb0a1812aa7e67a16805
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2025-06-23 14:59:53 +0900

    기본으로 사용할 Character 클래스 생성 및 AbilitySystem 초기화

---
+++ Source/ProjectF/Character/BaseCharacter.cpp
@@ -0,0 +1,54 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseCharacter.h"
+
+#include "ProjectF/AbilitySystem/BaseAbilitySystemComponent.h"
+#include "ProjectF/Player/BasePlayerState.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(BaseCharacter)
+
+ABaseCharacter::ABaseCharacter()
+{
+	
+}
+
+void ABaseCharacter::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+
+	InitAbilitySystem();
+}
+
+void ABaseCharacter::OnRep_PlayerState()
+{
+	Super::OnRep_PlayerState();
+
+	InitAbilitySystem();
+}
+
+UAbilitySystemComponent* ABaseCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+UBaseAbilitySystemComponent* ABaseCharacter::GetBaseAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+UBaseAttributeSet* ABaseCharacter::GetBaseAttributeSet() const
+{
+	return AttributeSet;
+}
+
+void ABaseCharacter::InitAbilitySystem()
+{
+	if (ABasePlayerState* BasePlayerState = GetPlayerState<ABasePlayerState>())
+	{
+		AbilitySystemComponent = BasePlayerState->GetBaseAbilitySystemComponent();
+		AbilitySystemComponent->InitAbilityActorInfo(BasePlayerState, this);
+		AttributeSet = BasePlayerState->GetBaseAttributeSet();
+	}
+}
+
