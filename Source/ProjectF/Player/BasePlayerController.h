ffbf36aaae15963a2dfddc882d81d956ca9d97cc
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2025-06-23 17:15:58 +0900

    기본으로 사용할 PlayerController 클래스 생성 - InputData를 이용해 IMC를 추가하고 Bind Input 수행

---
+++ Source/ProjectF/Player/BasePlayerController.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "GameFramework/PlayerController.h"
+#include "BasePlayerController.generated.h"
+
+class UBaseAbilitySystemComponent;
+class UInputData;
+
+UCLASS()
+class PROJECTF_API ABasePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	ABasePlayerController();
+	virtual void PostProcessInput(const float DeltaTime, const bool bGamePaused) override;
+	
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+
+private:
+	UPROPERTY()
+	TObjectPtr<UInputData> InputData;
+
+	void Input_AbilityInputTagPressed(FGameplayTag InputTag);
+	void Input_AbilityInputTagReleased(FGameplayTag InputTag);
+
+	UBaseAbilitySystemComponent* GetBaseAbilitySystemComponent() const;
+};
